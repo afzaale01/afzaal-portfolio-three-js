aef4fb88c7a96a5e5cb445abf3521a1070ccf362
Muhammad Afzaal <60592691+afzaale01@users.noreply.github.com>
2024-10-22 19:25:05 +0500

    pointer function

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,22 +13,43 @@ import {useFrame, useThree } from '@react-three/fiber';
 import {a} from '@react-spring/three';
 import islandScene from '../assets/3d/island.glb';
 
-const Island = (props) =>{
+const Island = ({isRotating, setIsRotating, ...props}) =>{
+  const islandRef = useRef();
+  const {gl, viewport} = useThree();
+  const { nodes, materials } = useGLTF(islandScene);
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
 
-    const islandRef = useRef();
+  const handlePointerDown = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(true);
 
-  const { nodes, materials } = useGLTF(islandScene)
+     const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+     lastX.current = clientX;
+
+  }
+
+  const handlePointerUp = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(flase);
+  }
+
+  const handlePointerMove = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+  }
+
+
   return (
     <a.group ref={islandRef} {...props} >
-      <mesh
-        
-        
+      <mesh       
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
-      <mesh
-        
-        
+      <mesh        
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />

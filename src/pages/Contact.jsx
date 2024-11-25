8b0d61a470822ae65b21cc698090d205c33675dc
Muhammad Afzaal <60592691+afzaale01@users.noreply.github.com>
2024-11-25 16:48:54 +0500

    Base mesh loading is completed. Now we move to finalize the repo

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, Suspense} from 'react'
 import emailjs from '@emailjs/browser'; 
+import { Canvas } from '@react-three/fiber';
+import Fox from "../models/Fox";
+import Loader from '../components/Loader';
+
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -113,6 +117,25 @@ const Contact = () => {
             </button>
 
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position:[0,0,5],
+            fov:75,
+            near:0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <Suspense fallback={<Loader />}>
+            <Fox
+            position={[0.5,0.35,0]}
+            rotation={[12.6,-0.6,0]}
+            scale={[0.5,0.5,0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
 
     </section>
